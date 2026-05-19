2fc146d4482b633ebc3b4aa3c9c10d1b801708f1
lkaydhillon <lkaydhillon@gmail.com>
2026-05-19 17:11:30 +0530

    rm networking host at local

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     port: 5174,
-    host: '0.0.0.0',
+    //host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
